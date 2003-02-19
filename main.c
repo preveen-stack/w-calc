316fb79dd265d611d0fa8d2e4376fae660ffbe48
Kyle Wheeler <kyle-wcalc@memoryhole.net>
2003-02-19 16:27:43 +0000

    Moved the help functions.

--- main.c
+++ main.c
@@ -41,8 +41,7 @@ extern int read_history ();
 #include "calculator.h"
 #include "parser.h"
 #include "variables.h"
-
-#define SUPPORTED_SYMBOLS "+ - () {} [] * %% / ! ^ > >= < <= == != && || << >>\nsin cos tan asin acos atan sinh cosh tanh asinh acosh atanh\nlog ln round abs ceil floor sqrt\ne pi Na k Cc ec R G g Me Mp Mn Md u amu c h mu0 epsilon0\nmuB muN b mW mZ ao F Vm NAk eV sigma alpha gamma re\n"
+#include "help.h"
 
 #ifdef HAVE_READLINE_HISTORY
 #define ADD_HISTORY(x) add_history(x)
@@ -174,9 +173,6 @@ int main (int argc, char *argv[])
 					ADD_HISTORY(readme);
 					yydebug = ! yydebug;
 					printf("Debug Mode %s\n",yydebug?"On":"Off");
-				} else if (!strncmp(readme,"?",1) || !strncmp(readme,"help",4)) {
-					ADD_HISTORY(readme);
-					print_interactive_help();
 				} else {
 					parseme(readme);
 					{
@@ -216,72 +212,4 @@ int main (int argc, char *argv[])
 	}
 
 	exit(0);
-}
-
-void print_interactive_help (void)
-{
-	printf("Wcalc %s by Kyle Wheeler.\nVersion 0.1.x written in C++ for CS240C at Ohio University, Jan 14 2000\n",VERSION);
-	printf("Version 0.2.x rewritten for lex/yacc in C, Nov 25 2001\n");
-	printf( "This calculator recognized the following symbols:\n"
-		 SUPPORTED_SYMBOLS
-		 "The ! is factorial or boolean, depending on use.\n"
-		 "Spaces are ignored (unknown symbols cause errors or are treated as variables)\n"
-		 "This calculator accepts input in decimal (1.00), scientific notation (1.0e5),\n"
-		 "octal (088), hexadecimal (0xff), and binary (0b11).\n\n"
-		 "\\px is the way to set the precision, where x is the desired precision.\n"
-		 "    This setting only affects output.\n"
-		 "\\e  toggles the formatting of output between decimal and scientific notation.\n"
-		 "    This setting only applies when precision is specified.\n"
-		 "a   in an expression represents the previous answer.\n"
-		 "\\q  exits the program.\n"
-		 "?   displays this help screen.\n"
-		 "\\radians toggles radian mode.\n"
-		 "\\commas toggles output delineation between commas and periods.\n"
-		 "\\p or \\picky toggles stricter variable parsing rules.\n"
-		 "\\r or \\radians toggles radian mode for sin-related functions.\n"
-		 "\\s or \\strict toggles stricter number parsing rules.\n"
-		 "\\dsepX sets the decimal separator character to X.\n"
-		 "\\tsepX sets the thousands-place separator character to X.\n"
-		 "\\dec, \\hex, \\oct, \\bin set the output format.\n"
-		 "\\cons or \\conservative toggles precision guards.\n"
-		 "\\li or \\list or \\listvars prints out the currently defined variables.\n\n"
-		 "Variables are supported and may be assigned using the = operator.\n"
-		 "To assign a variable, use the form:\n"
-		 "foo = anylegalexpression\n"
-		 "Thereafter, that variable name is the same as the literal value it represents.\n"
-		 );
-}
-
-void print_command_help (void)
-{
-	printf("Wcalc %s by Kyle Wheeler.\nVersion 0.1.x written in C++ for CS240C at Ohio University, Jan 14 2000\n",VERSION);
-	printf("Version 0.2.x rewritten for lex/yacc in C, Nov 25 2001\n\n");
-	printf( "This calculator recognized the following symbols:\n"
-		 SUPPORTED_SYMBOLS
-		 "\nThe ! is factorial or boolean, depending on use.\n"
-		 "Spaces are ignored (unknown symbols cause errors or are treated as variables)\n"
-		 "This calculator accepts input in decimal (1.00), scientific notation (1.0e5),\n"
-		 "octal (088), hexadecimal (0xff), and in binary (0b11).\n\n"
-		 "-v, --version            Prints the version number.\n"
-		 "-Px                      Sets the precision, where x is the desired precision.\n"
-		 "                         This setting only affects output.\n"
-		 "-E                       Toggles the formatting of output between decimal\n"
-		 "                         and scientific notation.\n"
-		 "                         This setting only applies when precision is specified.\n"
-		 "-d, -dec, --decimal      Prints the output in decimal form.\n"
-		 "-h, -hex, --hexadecimal  Prints the output in hexadeximal form.\n"
-		 "-o, -oct, --octal        Prints the output in octal form.\n"
-		 "-b, -bin, --binary       Prints the output in binary form.\n"
-		 "-p                       Prints the prefixes for hexadecimal, octal and\n"
-		 "                         binary forms.\n"
-		 "-l, --lenient            Makes the parser assume that uninitialized variables\n"
-		 "                         have a value of zero.\n"
-		 "-r, --radians            This makes all trigonometric functions work in radians.\n\n"
-		 "Variables are supported and may be assigned using the = operator.\n"
-		 "To assign a variable, use the form:\n"
-		 "foo = anylegalexpression\n"
-		 "Thereafter, that variable name is the same as the literal value it represents.\n\n"
-		 "Executing this program without any arguments enters an 'interactive' mode\n"
-		 "with a few more features and options.\n\n"
-		 );
 }
