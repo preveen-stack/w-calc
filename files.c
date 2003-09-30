b77d0a1177103ecd886f1fd78fc231d8e60230fa
Kyle Wheeler <kyle-wcalc@memoryhole.net>
2003-09-30 22:48:54 +0000

    Ran it through 'indent'

--- files.c
+++ files.c
@@ -7,13 +7,13 @@
  *
  */
 
-#include <fcntl.h>     /* for open() */
-#include <unistd.h>    /* for close() */
-#include <errno.h>     /* for errno */
-#include <sys/types.h> /* for open flags */
-#include <sys/stat.h>  /* for open flags */
-#include <string.h>    /* for strlen() */
-#include <stdlib.h>     /* for free() */
+#include <fcntl.h>					   /* for open() */
+#include <unistd.h>					   /* for close() */
+#include <errno.h>					   /* for errno */
+#include <sys/types.h>				   /* for open flags */
+#include <sys/stat.h>				   /* for open flags */
+#include <string.h>					   /* for strlen() */
+#include <stdlib.h>					   /* for free() */
 
 #include "historyManager.h"
 #include "string_manip.h"
@@ -23,29 +23,38 @@
 #include "files.h"
 
 /* this is for communicating with the scanner */
-char * open_file = NULL;
+char *open_file = NULL;
 
-int saveState(char* filename)
+int saveState(char *filename)
 {
-	int fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC,
+				  S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 	int return_error = 0;
+
 	free(filename);
-	if (fd >= 0) { // success
+	if (fd >= 0) {
+		//success
 		int hindex;
 		int retval;
 		extern int contents;
+
 		/* save variables */
-		for (hindex=0;hindex<contents;hindex++) {
+		for (hindex = 0; hindex < contents; hindex++) {
 			struct variable *keyval = getrealnvar(hindex);
 			char value[100], *cptr;
-			if (! keyval) continue;
-			if (!strcmp(keyval->key,"a")) continue;
-			retval = write(fd,keyval->key,strlen(keyval->key));
-			if (retval < (int) strlen(keyval->key)) {
+
+			if (!keyval) {
+				continue;
+			}
+			if (!strcmp(keyval->key, "a")) {
+				continue;
+			}
+			retval = write(fd, keyval->key, strlen(keyval->key));
+			if (retval < (int)strlen(keyval->key)) {
 				return_error = errno;
 				break;
 			}
-			retval = write(fd,"=",1);
+			retval = write(fd, "=", 1);
 			if (retval < 1) {
 				return_error = errno;
 				break;
@@ -53,40 +62,41 @@ int saveState(char* filename)
 			if (keyval->exp) {
 				cptr = keyval->expression;
 			} else {
-				sprintf(value,"%g",keyval->value);
+				sprintf(value, "%g", keyval->value);
 				cptr = value;
 			}
-			retval = write(fd,cptr,strlen(cptr));
-			if (retval < (int) strlen(cptr)) {
+			retval = write(fd, cptr, strlen(cptr));
+			if (retval < (int)strlen(cptr)) {
 				return_error = errno;
 				break;
 			}
-			retval = write(fd,"\n",1);
+			retval = write(fd, "\n", 1);
 			if (retval < 1) {
 				return_error = errno;
 				break;
 			}
 		}
-	    /* save history */
-		for (hindex=0;hindex<historyLength();hindex++) {
-			char * history_entry = historynum(hindex,1);
-			retval = write(fd,history_entry,strlen(history_entry));
-			if (retval < (int) strlen(history_entry)) {
+		/* save history */
+		for (hindex = 0; hindex < historyLength(); hindex++) {
+			char *history_entry = historynum(hindex, 1);
+
+			retval = write(fd, history_entry, strlen(history_entry));
+			if (retval < (int)strlen(history_entry)) {
 				return_error = errno;
 				break;
 			}
-			retval = write(fd," # = ",5);
+			retval = write(fd, " # = ", 5);
 			if (retval < 5) {
 				return_error = errno;
 				break;
 			}
-			history_entry = historynum(hindex,2);
-			retval = write(fd,history_entry,strlen(history_entry));
-			if (retval < (int) strlen(history_entry)) {
+			history_entry = historynum(hindex, 2);
+			retval = write(fd, history_entry, strlen(history_entry));
+			if (retval < (int)strlen(history_entry)) {
 				return_error = errno;
 				break;
 			}
-			retval = write(fd,"\n",1);
+			retval = write(fd, "\n", 1);
 			if (retval < 1) {
 				return_error = errno;
 				break;
@@ -95,32 +105,36 @@ int saveState(char* filename)
 		if (close(fd) != 0) {
 			return_error = errno;
 		}
-	} else { // failure
+	} else {
+		//failure
 		return_error = errno;
 	}
 	return return_error;
 }
 
-int loadState (char* filename)
+int loadState(char *filename)
 {
 	int fd, return_error = 0;
 	int standard_output_save = standard_output;
 
 	standard_output = 0;
 	fd = open(filename, O_RDONLY);
-	if (fd >= 0) { // success
-		char * linebuf;
+	if (fd >= 0) {
+		//success
+		char *linebuf;
 		int retval;
 		double val;
 		unsigned int linelen = 0, maxlinelen = 99;
 
-		linebuf = calloc(sizeof(char),100);
-		retval = read(fd,linebuf+linelen,1);
+		linebuf = calloc(sizeof(char), 100);
+		retval = read(fd, linebuf + linelen, 1);
 		while (retval == 1) {
 			while (retval == 1 && linebuf[linelen] != '\n') {
 				linelen++;
 				if (linelen == maxlinelen) {
-					char * newlinebuf = realloc( linebuf, sizeof(char)*(maxlinelen+100));
+					char *newlinebuf = realloc(linebuf,
+											   sizeof(char) * (maxlinelen +
+															   100));
 					if (newlinebuf) {
 						maxlinelen += 100;
 						linebuf = newlinebuf;
@@ -130,25 +144,27 @@ int loadState (char* filename)
 						break;
 					}
 				}
-				retval = read(fd,linebuf+linelen,1);
+				retval = read(fd, linebuf + linelen, 1);
 			}
 			linebuf[linelen] = 0;
-			strstrip(' ',linebuf);
+			strstrip(' ', linebuf);
 			stripComments(linebuf);
 			if (strlen(linebuf)) {
-				extern char * errstring;
+				extern char *errstring;
 				char *safe;
+
 				safe = strdup(linebuf);
 				val = parseme(safe);
-				putval("a",val);
-				if (!errstring || (errstring && !strlen(errstring)) || conf.remember_errors) {
+				putval("a", val);
+				if (!errstring || (errstring && !strlen(errstring)) ||
+					conf.remember_errors) {
 					addToHistory(linebuf, val);
 				}
 			}
 			linelen = 0;
-			memset(linebuf,0,maxlinelen);
+			memset(linebuf, 0, maxlinelen);
 			if (retval == 1) {
-				retval = read(fd,linebuf+linelen,1);
+				retval = read(fd, linebuf + linelen, 1);
 			}
 		}
 
@@ -161,4 +177,3 @@ int loadState (char* filename)
 	standard_output = standard_output_save;
 	return return_error;
 }
-		
