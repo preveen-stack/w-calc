fa5b66f8f594adce88586600356cebedc9ca6334
Kyle Wheeler <kyle-wcalc@memoryhole.net>
2006-02-21 16:07:06 +0000

    unnecessary function

--- historyManager.c
+++ historyManager.c
@@ -29,7 +29,6 @@
 extern void add_history();
 extern int write_history();
 extern int read_history();
-extern int history_truncate_file(char *, int);
 # endif	/* defined(HAVE_READLINE_HISTORY_H) */
 /* no history */
 #endif /* HAVE_READLINE_HISTORY */
