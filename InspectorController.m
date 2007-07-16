5a567a63798a7c5380a74c79561b2c13e7132e2d
Kyle Wheeler <kyle-wcalc@memoryhole.net>
2007-07-16 23:22:34 +0000

    whitespace

--- InspectorController.m
+++ InspectorController.m
@@ -7,37 +7,37 @@
 
 - (IBAction)closeIt:(id)sender
 {
-	[inspectorWindow close];
+    [inspectorWindow close];
 }
 
 - (IBAction)openIt:(id)sender
 {
-	NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
-	[prefs setObject:@"YES" forKey:@"historyShowing"];
-	[affectInspectorMenu setTitle:@"Hide Inspector"];
-	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onClose:) name:NSWindowWillCloseNotification object:inspectorWindow];
-	[variableList reloadData];
-	[historyList reloadData];
-	[inspectorWindow setBecomesKeyOnlyIfNeeded:YES];
-	[inspectorWindow orderFront:self];
+    NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
+    [prefs setObject:@"YES" forKey:@"historyShowing"];
+    [affectInspectorMenu setTitle:@"Hide Inspector"];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onClose:) name:NSWindowWillCloseNotification object:inspectorWindow];
+    [variableList reloadData];
+    [historyList reloadData];
+    [inspectorWindow setBecomesKeyOnlyIfNeeded:YES];
+    [inspectorWindow orderFront:self];
 }
 
 - (IBAction)toggleIt:(id)sender
 {
-	if ([inspectorWindow isVisible]) {
-		[self closeIt:sender];
-	} else {
-		[self openIt:sender];
-	}	
+    if ([inspectorWindow isVisible]) {
+	[self closeIt:sender];
+    } else {
+	[self openIt:sender];
+    }
 }
 
 - (IBAction)onClose:(id)sender
 {
-	NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
+    NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
 
-	[[NSNotificationCenter defaultCenter] removeObserver:self name:NSWindowWillCloseNotification object:inspectorWindow];
-	[prefs setObject:@"NO" forKey:@"historyShowing"];
-	[affectInspectorMenu setTitle:@"Show Inspector"];	
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:NSWindowWillCloseNotification object:inspectorWindow];
+    [prefs setObject:@"NO" forKey:@"historyShowing"];
+    [affectInspectorMenu setTitle:@"Show Inspector"];
 }
 
 @end
