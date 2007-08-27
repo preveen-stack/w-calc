7e3f3c3d3ac9b63b64d4abc067edca36008d0818
Kyle Wheeler <kyle-wcalc@memoryhole.net>
2007-08-27 20:55:17 +0000

    vastly expanded non-mpfr functions... dont quite work right yet, but its almost there

--- number.h
+++ number.h
@@ -4,7 +4,9 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #else
-#define HAVE_LIBMPFR
+# ifndef HAVE_LIBMPFR
+#  define HAVE_LIBMPFR
+# endif
 #endif
 
 #ifdef HAVE_LIBMPFR
@@ -13,8 +15,10 @@
 # include <limits.h> /* MPFR uses ULONG_MAX on some systems */
 #endif
 
+#ifndef S_SPLINT_S /* splint barfs on the gmp.h header */
 #include <gmp.h>
 #include <mpfr.h>
+#endif
 
 #define Number mpfr_t
 #define NUM_PREC_MIN MPFR_PREC_MIN
@@ -28,8 +32,11 @@
 
 #define num_free(n) mpfr_clear(n)
 
+typedef mp_exp_t num_exp_t;
 #define num_get_d(n) mpfr_get_d((n), GMP_RNDN)
 #define num_get_ui(n) mpfr_get_ui((n), GMP_RNDN)
+#define num_get_str(a, b, c, d, e) mpfr_get_str(a, b, c, d, e, GMP_RNDN)
+#define num_free_str(a) mpfr_free_str(a)
 
 #define num_set(n1,n2) mpfr_set((n1), (n2), GMP_RNDN)
 #define num_set_d(n,d) mpfr_set_d((n), (d), GMP_RNDN)
@@ -90,6 +97,7 @@
     mpfr_asinh((ret), (n), GMP_RNDN); \
 } while (0)
 
+extern gmp_randstate_t randstate;
 #define num_random(n) mpfr_urandomb((n), randstate)
 #define num_zeta(ret,n) mpfr_zeta((ret), (n), GMP_RNDN);
 #define num_sinc(ret,n) do { \
@@ -119,7 +127,7 @@
 
 #define num_cmp_si(n,si) mpfr_cmp_si((n), (si))
 
-#define num_prec_t mp_prec_t
+typedef mp_prec_t num_prec_t;
 #define num_get_default_prec() mpfr_get_default_prec()
 #define num_set_default_prec(prec) mpfr_set_default_prec(prec)
 
@@ -219,11 +227,121 @@
 # define num_bnot(ret,n) mpfr_set_ui((ret),~mpfr_get_ui((n),GMP_RNDN),GMP_RNDN)
 #endif
 
-#else
-typedef struct numberstruct Number;
+#else /* this is to reimplement mpfr using doubles */
+#include <float.h> /* for DBL_MANT_DIG, according to C89 */
+#include <stddef.h> /* for size_t, according to C89 */
+#include <stdio.h> /* for FILE* */
+struct numberstruct {
+    double value;
+    unsigned int nan;
+};
+typedef struct numberstruct Number[1];
+
+#define NUM_PREC_MIN DBL_MANT_DIG
+#define NUM_PREC_MAX DBL_MANT_DIG
+
+typedef int num_exp_t;
+typedef long int num_prec_t;
+
+void num_init(Number n);
+void num_init_set(Number n1, const Number n2);
+void num_init_set_d(Number n, const double d);
+void num_init_set_ui(Number n, const unsigned int ui);
+int num_init_set_str(Number n, const char * str, const int base);
+
+void num_set(Number n1, const Number n2);
+void num_set_d(Number n, const double d);
+void num_set_ui(Number n, const unsigned int ui);
+int num_set_str(Number n, const char * str, const int base);
+void num_set_nan(Number n);
+
+void num_mul(Number ret, const Number n1, const Number n2);
+void num_mul_si(Number ret, const Number n, const int si);
+void num_mul_ui(Number ret, const Number n, const unsigned int ui);
+void num_sqr(Number ret, const Number n);
+void num_sqrt(Number ret, const Number n);
+void num_cbrt(Number ret, const Number n);
+
+void num_pow(Number ret, const Number n, const Number exp);
+void num_pow_si(Number ret, const Number n, const int exp);
+void num_exp(Number ret, const Number n);
+void num_factorial(Number ret, unsigned int op);
+void num_log10(Number ret, const Number n);
+void num_log2(Number ret, const Number n);
+void num_log(Number ret, const Number n);
+
+void num_sin(Number ret, const Number n);
+void num_cos(Number ret, const Number n);
+void num_tan(Number ret, const Number n);
+void num_asin(Number ret, const Number n);
+void num_acos(Number ret, const Number n);
+void num_atan(Number ret, const Number n);
+void num_sinh(Number ret, const Number n);
+void num_cosh(Number ret, const Number n);
+void num_tanh(Number ret, const Number n);
+void num_asinh(Number ret, const Number n);
+void num_acosh(Number ret, const Number n);
+void num_atanh(Number ret, const Number n);
+void num_acoth(Number ret, const Number n);
+void num_asech(Number ret, const Number n);
+void num_acsch(Number ret, const Number n);
+
+void num_add(Number ret, const Number n1, const Number n2);
+void num_sub(Number ret, const Number n1, const Number n2);
+void num_add_ui(Number ret, const Number n, const unsigned int ui);
+void num_sub_ui(Number ret, const Number n, const unsigned int ui);
+
+void num_div(Number ret, const Number n1, const Number n2);
+void num_div_ui(Number ret, const Number n, const unsigned int ui);
+
+void num_rint(Number ret, const Number n);
+void num_rintz(Number ret, const Number n);
+void num_floor(Number ret, const Number n);
+void num_ceil(Number ret, const Number n);
+
+int num_random(Number n);
+#define num_zeta(ret,n) num_unimplemented()
+void num_sinc(Number ret, const Number n);
+
+void num_const_pi(Number n);
+void num_const_euler(Number n);
+void num_const_catalan(Number n);
+
+void num_out_str(FILE *fd, const int base, const Number n);
+int num_is_nan(const Number n);
+int num_is_inf(const Number n);
+int num_is_zero(const Number n);
+int num_is_equal(const Number n1, const Number n2);
+int num_is_greater(const Number n1, const Number n2);
+int num_is_greaterequal(const Number n1, const Number n2);
+int num_is_less(const Number n1, const Number n2);
+int num_is_lessequal(const Number n1, const Number n2);
+int num_sign(const Number n);
+void num_neg(Number ret, const Number n);
+void num_abs(Number ret, const Number n);
+int num_cmp_si(const Number n, const int si);
 
 void num_free(Number);
-double num_get_d(Number);
+double num_get_d(const Number);
+unsigned int num_get_ui(const Number);
+char * num_get_str(char *str, num_exp_t *expptr, const int b, const size_t n, const Number op);
+#define num_free_str(a) free(a);
+num_prec_t num_get_default_prec();
+#define num_set_default_prec(prec) /* num_set_default_prec doesn't exist */
+void num_cot(Number ret, const Number n);
+void num_sec(Number ret, const Number n);
+void num_csc(Number ret, const Number n);
+void num_coth(Number ret, const Number n);
+void num_sech(Number ret, const Number n);
+void num_csch(Number ret, const Number n);
+#define num_lngamma(ret, n) num_unimplemented()
+#define num_gamma(ret,n) num_unimplemented()
+#define num_eint(ret,n) num_unimplemented()
+void num_comp(Number ret, const Number n);
+void num_bnot(Number ret, const Number n);
+void num_band(Number ret, const Number n1, const Number n2);
+void num_bxor(Number ret, const Number n1, const Number n2);
+void num_bor(Number ret, const Number n1, const Number n2);
 #endif
 
 void init_numbers(void);
